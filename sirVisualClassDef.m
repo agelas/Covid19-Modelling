82d8f01bd11a0d2411a932505810d3594d19f126
Mathias <mathiaswi177@gmail.com>
2020-04-21 23:26:28 -0700

    Visualization engine for SIR model. I *think* most of the basic setup is done, have to figure out how update infected population in a way consistent with growth rate of SIR model, and also find a way to plot the data.

---
+++ sirVisualClassDef.m
@@ -0,0 +1,195 @@
+
+classdef sirVisualClassDef < handle
+    properties
+        N;          %number of people
+        L;          %size of simulation box
+        t;          %current number of time steps completed
+        dt;         %time step size
+        otime;      %number of time steps between outputs
+        rc=1;       %cutoff distance for interaction
+        pos;        %positions of the atoms in Nx2 array
+        vel;        %velocities of the atoms in Nx2 array
+        m=1;        %masses of each atom
+        phand;      %list of hadles to all circles representing people
+        timeser;    %list of times at which data is recorded
+        Eser;       %the energy per atom 
+        KEser;      %the kinetic energy per atom
+        PEser;      %potential energy per atom
+        KEfix;
+    end
+    
+    methods
+        %constructor
+        function obj=sirVisualClassDef(N,L,KE) %KE should be between 0.1-0.8 so like 0.5 probably
+            obj.N=N;
+            obj.L=L;
+            avg=KE*2;
+            obj.t=0;
+            obj.dt=0.01;                        %setting the time step
+            [posx,posy]=meshgrid([1:sqrt(N)],[1:sqrt(N)]);  %meshgrid for people indices
+            posx=reshape(posx,1,N);            %reformatting x positions 
+            posy=reshape(posy,1,N);            %reformatting y positions
+            obj.pos=[posx',posy'];              %filling in pos array
+            obj.pos=obj.pos-0.5;                %shifting indexes to not fall on whole numbers
+            obj.vel=randn(obj.N,2)*sqrt(avg);   %distributing random velocities across the people
+            
+        end
+        
+        function obj=step(obj)
+            velocityFirst=obj.vel;              %filling in velocityFirst for movement calculations
+            
+            for i=1:obj.N                       %looping through all other people in simulation
+                
+                obj.pos(i,1)=obj.pos(i,1)+obj.dt*obj.vel(i,1);  %updating x coordinate
+                obj.pos(i,2)=obj.pos(i,2)+obj.dt*obj.vel(i,2);  %updating y coordinate
+                
+                rij=0;  %radius to atoms within boundary contributing to force
+                rijx=0; %x-distance to atoms within boundary
+                rijy=0; %y-distance to atoms within boundary
+                
+                Fix=0;
+                Fiy=0;
+                
+                potentialEnormal=0; %potential energy from within boundaries
+                
+                if obj.pos(i,1)<0   %Checking boundary conditions
+                    obj.pos(i,1)=obj.pos(i,1)+obj.L;
+                end
+                
+                if obj.pos(i,2)<0   %Checking boundary conditions
+                    obj.pos(i,2)=obj.pos(i,2)+obj.L;
+                end
+
+                if obj.pos(i,1)>obj.L   %Checking boundary conditions
+                    obj.pos(i,1)=obj.pos(i,1)-obj.L;
+                end
+
+                if obj.pos(i,2)>obj.L   %Checking boundary conditions
+                    obj.pos(i,2)=obj.pos(i,2)-obj.L;
+                end
+                
+                for j=1:obj.N %Looping through all other points for distance calcs
+                       
+                    rijx(j)= obj.pos(j,1)-obj.pos(i,1); %x distance is recorded
+                    rijy(j)= obj.pos(j,2)-obj.pos(i,2); %y distance is recorded
+                    
+                    
+                    if rijx(j)>obj.L/2  %past boundary forces
+                        rijx(j)=rijx(j)-obj.L;
+                    
+                    elseif rijx(j)<-obj.L/2  %past boundary force
+                        rijx(j)=rijx(j)+obj.L;
+                        
+                    end
+                    
+                    if rijy(j)>obj.L/2  %past boundary forces
+                        rijy(j)=rijy(j)-obj.L; 
+                       
+                    elseif rijy(j)<-obj.L/2  %past boundary forces
+                        rijy(j)=rijy(j)+obj.L;
+                        
+                    end
+                    
+                    if sqrt((rijx(j))^2+(rijy(j)^2))<=obj.rc   %Checking if distance is within 2.5
+                        rij(j)=sqrt((rijx(j))^2+(rijy(j)^2));  %If within range, distance is recorded 
+                    else
+                        rij(j)=0;
+                    end
+                    
+                    if rij(j)==0    %if radius is 0, x and y distances are set to 0 to discard atom
+                        Fix(j)=0;
+                        Fiy(j)=0;
+                    else
+                        Fix(j)=24*((2*rij(j))^-14-(rij(j))^-8)*rijx(j); %Potential energy calculation in x
+                        Fiy(j)=24*((2*rij(j))^-14-(rij(j))^-8)*rijy(j); %Potential energy calculation in y
+                    end    
+                end
+                
+                %finding total potential energy for the i atom
+                for k=1:length(rij)
+                    if rij(k)==0
+                        potentialEnormal(k)=0;
+                    else
+                        potentialEnormal(k)=4*(rij(k).^-12-rij(k).^-6-obj.rc^-12+obj.rc^-6);
+                    end
+                end
+                
+                PE(i)=sum(potentialEnormal)/36; %summing all contributions for total potential energy
+
+                Fix=sum(Fix);   %summing all the x forces
+                Fiy=sum(Fiy);   %summing all the y forces
+
+                obj.vel(i,1)=obj.vel(i,1)+obj.dt*Fix;   %updating x velocity
+                obj.vel(i,2)=obj.vel(i,2)+obj.dt*Fiy;   %updating y velocity
+                
+                velXavg=mean(obj.vel(:,1));             %Calculating average velocity in x
+                velYavg=mean(obj.vel(:,2));             %Calculating average veloctiy in y
+                
+                if obj.KEfix==1                         %KEfix
+                    for i=1:obj.N
+                        obj.vel(i,1)=(obj.vel(i,1)-velXavg)*(1/(sqrt(obj.vel(i,1)^2+obj.vel(i,2)^2)));
+                        obj.vel(i,2)=(obj.vel(i,2)-velYavg)*(1/(sqrt(obj.vel(i,1)^2+obj.vel(i,2)^2)));
+                    end
+                end
+
+                obj.pos(i,1)=obj.pos(i,1)+obj.dt*obj.vel(i,1);  %updating x coordinate
+                obj.pos(i,2)=obj.pos(i,2)+obj.dt*obj.vel(i,2);  %updating y coordinate
+                
+            end
+            velocitySecond=obj.vel; %second velocity to hold updated velocity for kinetic evergy calculations
+            
+            velocityMean=(velocityFirst+velocitySecond)./2; %taking the mean of the velocities
+            
+            netVelocity=sqrt(velocityMean(:,1).^2+velocityMean(:,2).^2);   %taking the net velocity 
+            
+            KE=0.5.*(netVelocity.^2);   %calculatingn KE accoring to 0.5mv^2
+            KE=KE';                     %transposing KE so it adds properly
+            
+            Energy(i)=PE(i)+KE(i);      %Calculating total energy
+            
+            obj.KEser(end+1)=mean(mean(KE)); %filling in KEser array
+            
+            obj.PEser(end+1)=mean(PE);       %filling in PEser array
+            
+            obj.Eser(end+1)=mean(Energy);    %filling in Eser array
+            
+            obj.timeser(end+1)=obj.t;        %filling in timeser array
+            obj.t=obj.t+obj.dt;              %updating current time
+        end
+    
+    
+        function [k,p,e]=draw(obj)
+            subplot(1,1,1);
+            set(gca,'Color','k','XTick',[],'YTick',[]);
+            
+            counter=1;
+            while obj.t<2
+                %subplot(2,1,1);
+
+                obj.step;
+                if counter==1
+                    for i=1:obj.N %converting points to rounded rectangles
+                        obj.phand(i)=rectangle('Position', [obj.pos(i,1)-0.5 obj.pos(i,2)-0.5 0.3 0.3],'Curvature',[1 1],'FaceColor','b','EdgeColor','k');  
+                    end
+                else
+                    for i=1:obj.N %changing position of rectangles on every iteration
+                        set(obj.phand(i),'Position', [obj.pos(i,1)-0.5 obj.pos(i,2)-0.5 0.3 0.3]);
+                        r = (10)*rand(1);
+                        if (r > 9.8)
+                            set(obj.phand(i),'FaceColor','r');
+                        end
+                    end
+                end
+                counter=counter+1;
+                xlim([0 obj.L]);    %setting dimensions of display
+                ylim([0 obj.L]);    %setting dimensions of display
+                    
+                %subplot(2,1,2)
+                %pause(0.05) %uncomment to make runtime longer, or add to
+                %max time also
+                drawnow %THIS CALL IS IMPORTANT IT CONTROLS ALL CALLBACKS
+                
+            end
+        end
+    end
+end
